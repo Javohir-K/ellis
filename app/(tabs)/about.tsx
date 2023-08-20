c75e4ef5ce60d24e6460acde2e9489326ca31914
Javohir-K <javoxir2000k@gmail.com>
2023-08-20 14:53:09 +0500

    update bout

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -5,7 +5,6 @@ export default function about() {
   return (
     <View>
       <Text>This app is for english learners of all levels.</Text>
-      <Text>I am glad you chose my application arigatou `(uwu)`</Text>
     </View>
   )
 }
