04439bf22c9921c6c411c2b3f9ab9b56704566cd
Javohir-K <javoxir2000k@gmail.com>
2023-08-20 14:41:15 +0500

    v1.0

--- api/firebase.ts
+++ api/firebase.ts
@@ -2,7 +2,6 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
-// import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCH2ERieoEKC64xM_hiBn9rkoyYDcq7JLc",
@@ -13,17 +12,11 @@ const firebaseConfig = {
   appId: "1:749422146569:web:25bc353848adfc03ab74d8",
   measurementId: "G-NCD8P89K2K",
 
-  // apiKey: "AIzaSyCXuK2NsR_XxBWRk-lKciXNNRwCO5az0pY",
-  // authDomain: "telegram-alt.firebaseapp.com",
-  // projectId: "telegram-alt",
-  // storageBucket: "telegram-alt.appspot.com",
-  // messagingSenderId: "1024315381789",
-  // appId: "1:1024315381789:web:bdcb10a8ada1ed72cf8ef4",
+
 };
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
-// const auth = getAuth();
 const db = app.firestore();
 
 export { db };
--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -4,7 +4,8 @@ import React from 'react'
 export default function about() {
   return (
     <View>
-      <Text>about</Text>
+      <Text>This app is for english learners of all levels.</Text>
+      <Text>I am glad you chose my application arigatou `(uwu)`</Text>
     </View>
   )
 }
